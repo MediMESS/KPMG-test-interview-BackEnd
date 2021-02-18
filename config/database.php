634547eedeba2f1294f087e0827b7cc27792cb77
Mehdi Messarat <medimessarat@gmail.com>
2021-02-18 11:17:10 +0100

    Tryiing to change the code for localhost and heroku db acess in one scrip

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL = parse_url('postgres://nouhudcndmnvov:39d782168d2f7998233cbe596565450afb618e1d7d3b97a4790127fe173092e6@ec2-54-225-130-212.compute-1.amazonaws.com:5432/d58ff1b7ltiudf');
+$DATABASE_URL = parse_url($_SERVER['DATABASE_URL']);
 
 $db_env = "";
 if ($_SERVER["APP_ENV"] == "production") {
@@ -11,7 +11,8 @@ if ($_SERVER["APP_ENV"] == "production") {
     $db_env = env('DB_DATABASE', 'forge');
 }
 
-error_log($_SERVER, $db_env);
+error_log($_SERVER);
+error_log($db_env);
 return [
 
     /*
